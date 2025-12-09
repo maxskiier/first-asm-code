93761760e1ea328e89a1a53beadfaaf2ab6e8131
Max <m32285159@gmail.com>
2025-12-08 23:10:46 -0600

    Windows version of make.sh
    PLEASE NOTE!!!!
    THIS IS UNTESTED SINCE I AM NOT WRITING THIS ON A WINDOWS MACHINE!

---
+++ make.bat
@@ -0,0 +1,23 @@
+@echo off
+:start
+cls
+set /p C02choice="Do you want to make this in 65C02 mode? (y/n):"
+
+if /I "%C02choice%"=="y" goto :C02mode
+if /I "%C02choice%"=="n" goto :regmode
+
+echo Invalid input.
+pause
+goto :start
+
+:C02mode
+echo 65C02 mode selected
+.\ca65 program.s -D C02
+.\ld65 program.o -C program.cfg
+goto :eof
+
+:regmode
+echo 6502 mode selected
+.\ca65 program.s
+.\ld65 program.o -C program.cfg
+goto :eof
