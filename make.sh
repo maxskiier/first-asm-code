98f4c299945270b2be293a9699ef330da5672f1c
maxskiier <m32285159@gmail.com>
2025-12-12 10:28:28 -0600

    Added option for other directories

--- make.sh
+++ make.sh
@@ -4,15 +4,17 @@ while true; do
     read -n 1 -p "Use 65C02 mode? (y/n): " ans
 
     if [[ "$ans" == "y" || "$ans" == "Y" ]]; then
-        echo "65C02 mode selected"
-        ./ca65 defines.s -D C02
-        ./ld65 defines.o -C program.cfg
+        printf "\n65C02 mode selected\n"
+	read -p "What is the directory of ca65 and ld65? " dir
+        "$dir"/ca65 ./defines.s -D C02
+        "$dir"/ld65 ./defines.o -C program.cfg
         break
 
     elif [[ "$ans" == "n" || "$ans" == "N" ]]; then
-        echo "6502 mode selected"
-        ./ca65 defines.s
-        ./ld65 defines.o -C program.cfg
+        printf "\n6502 mode selected\n"
+        read -p "What is the directory of ca65 and ld65? " dir
+	"$dir"/ca65 ./defines.s
+        "$dir"/ld65 ./defines.o -C program.cfg
         break
 
     else
